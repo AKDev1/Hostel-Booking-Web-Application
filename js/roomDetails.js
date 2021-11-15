51d0708cc5da5bbb238eb15d51957166f98ff1cf
AKDev1 <60657206+AKDev1@users.noreply.github.com>
2021-11-15 22:40:24 +0530

    Update roomDetails.js

--- js/roomDetails.js
+++ js/roomDetails.js
@@ -1,12 +1,12 @@
 function showMore1(){
   if(document.getElementById("hid1").style.display == "none"){
     document.getElementById("hid1").style.display = "table";
-    document.getElementById('roomplus1').src = "../assets/minus.png";
+    document.getElementById('roomplus1').src = "assets/minus.png";
     document.getElementById('roomplus1').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid1").style.display = "none";
-    document.getElementById('roomplus1').src = "../assets/plus.png";
+    document.getElementById('roomplus1').src = "assets/plus.png";
     document.getElementById('roomplus1').style.paddingTop = "0";
   }
 }
@@ -14,12 +14,12 @@ function showMore1(){
 function showMore2(){
   if(document.getElementById("hid2").style.display == "none"){
     document.getElementById("hid2").style.display = "table";
-    document.getElementById('roomplus2').src = "../assets/minus.png";
+    document.getElementById('roomplus2').src = "assets/minus.png";
     document.getElementById('roomplus2').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid2").style.display = "none";
-    document.getElementById('roomplus2').src = "../assets/plus.png";
+    document.getElementById('roomplus2').src = "assets/plus.png";
     document.getElementById('roomplus2').style.paddingTop = "0";
   }
 }
@@ -27,12 +27,12 @@ function showMore2(){
 function showMore3(){
   if(document.getElementById("hid3").style.display == "none"){
     document.getElementById("hid3").style.display = "table";
-    document.getElementById('roomplus3').src = "../assets/minus.png";
+    document.getElementById('roomplus3').src = "assets/minus.png";
     document.getElementById('roomplus3').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid3").style.display = "none";
-    document.getElementById('roomplus3').src = "../assets/plus.png";
+    document.getElementById('roomplus3').src = "assets/plus.png";
     document.getElementById('roomplus3').style.paddingTop = "0";
   }
 }
@@ -40,12 +40,12 @@ function showMore3(){
 function showMore4(){
   if(document.getElementById("hid4").style.display == "none"){
     document.getElementById("hid4").style.display = "table";
-    document.getElementById('roomplus4').src = "../assets/minus.png";
+    document.getElementById('roomplus4').src = "assets/minus.png";
     document.getElementById('roomplus4').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid4").style.display = "none";
-    document.getElementById('roomplus4').src = "../assets/plus.png";
+    document.getElementById('roomplus4').src = "assets/plus.png";
     document.getElementById('roomplus4').style.paddingTop = "0";
   }
 }
@@ -53,12 +53,12 @@ function showMore4(){
 function showMore5(){
   if(document.getElementById("hid5").style.display == "none"){
     document.getElementById("hid5").style.display = "table";
-    document.getElementById('roomplus5').src = "../assets/minus.png";
+    document.getElementById('roomplus5').src = "assets/minus.png";
     document.getElementById('roomplus5').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid5").style.display = "none";
-    document.getElementById('roomplus5').src = "../assets/plus.png";
+    document.getElementById('roomplus5').src = "assets/plus.png";
     document.getElementById('roomplus5').style.paddingTop = "0";
   }
 }
@@ -66,12 +66,12 @@ function showMore5(){
 function showMore6(){
   if(document.getElementById("hid6").style.display == "none"){
     document.getElementById("hid6").style.display = "table";
-    document.getElementById('roomplus6').src = "../assets/minus.png";
+    document.getElementById('roomplus6').src = "assets/minus.png";
     document.getElementById('roomplus6').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid6").style.display = "none";
-    document.getElementById('roomplus6').src = "../assets/plus.png";
+    document.getElementById('roomplus6').src = "assets/plus.png";
     document.getElementById('roomplus6').style.paddingTop = "0";
   }
 }
@@ -79,12 +79,12 @@ function showMore6(){
 function showMore7(){
   if(document.getElementById("hid7").style.display == "none"){
     document.getElementById("hid7").style.display = "table";
-    document.getElementById('roomplus7').src = "../assets/minus.png";
+    document.getElementById('roomplus7').src = "assets/minus.png";
     document.getElementById('roomplus7').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid7").style.display = "none";
-    document.getElementById('roomplus7').src = "../assets/plus.png";
+    document.getElementById('roomplus7').src = "assets/plus.png";
     document.getElementById('roomplus7').style.paddingTop = "0";
   }
 }
@@ -92,12 +92,12 @@ function showMore7(){
 function showMore8(){
   if(document.getElementById("hid8").style.display == "none"){
     document.getElementById("hid8").style.display = "table";
-    document.getElementById('roomplus8').src = "../assets/minus.png";
+    document.getElementById('roomplus8').src = "assets/minus.png";
     document.getElementById('roomplus8').style.paddingTop = "15px";
   }
   else{
     document.getElementById("hid8").style.display = "none";
-    document.getElementById('roomplus8').src = "../assets/plus.png";
+    document.getElementById('roomplus8').src = "assets/plus.png";
     document.getElementById('roomplus8').style.paddingTop = "0";
   }
 }
